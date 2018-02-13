363c99b76ad291de1cd4564fd525ac65df30195e
hexxcointakeover <hexxcointakeover@users.noreply.github.com>
2018-02-13 23:00:19 +0200

    adjust, no editor this time..

--- src/qt/splashscreen.cpp
+++ src/qt/splashscreen.cpp
@@ -69,7 +69,7 @@ SplashScreen::SplashScreen(Qt::WindowFlags f, const NetworkStyle *networkStyle) 
     pixPaint.fillRect(rGradient, gradient);
 
     // draw the bitcoin icon, expected size of PNG: 1024x1024
-    QRect rectIcon(QPoint(50,25), QSize(128,128));
+    QRect rectIcon(QPoint(45,30), QSize(128,128));
 
     const QSize requiredSize(1024,1024);
     QPixmap icon(networkStyle->getAppIcon().pixmap(requiredSize));
