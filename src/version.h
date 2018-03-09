4be6733ecb498391ec6f44604887c5d65fd40f7c
hexxcointakeover <noreply@mail.com>
2018-03-09 17:51:42 +0200

    Xnode sync fixed

--- src/version.h
+++ src/version.h
@@ -9,7 +9,7 @@
  * network protocol versioning
  */
 
-static const int PROTOCOL_VERSION = 99018;
+static const int PROTOCOL_VERSION = 99019;
 
 //! initial proto version, to be increased after version/verack negotiation
 static const int INIT_PROTO_VERSION = 99008;
