e061ee47fa19d4b9a09c7efef1a1256b62fad421
hexxcointakeover <noreply@mail.com>
2018-03-18 00:08:25 +0200

    ready

--- src/version.h
+++ src/version.h
@@ -21,7 +21,7 @@ static const int GETHEADERS_VERSION = 99013;
 static const int MIN_PEER_PROTO_VERSION = 99017;
 
 //! disconnect from all older peers after Xnode payment HF
-static const int MIN_PEER_PROTO_VERSION_AFTER_XNODE_PAYMENT_HF = 99020;
+static const int MIN_PEER_PROTO_VERSION_AFTER_XNODE_PAYMENT_HF = 99021;
 
 //! nTime field added to CAddress, starting with this version;
 //! if possible, avoid requesting addres99011ses nodes older than this
